150e06ca3297a7ffe0fcd118349244db993cef86
wt <wt@example.com>
2021-05-02 22:02:25 +0700

    Update

---
+++ clab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int i;
+    unsigned int cnt = 7;
+    for (i = cnt-2; i < cnt; i--)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
